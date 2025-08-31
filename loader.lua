4e363185b08e0df4e040a60267d95c90cfaa50a8
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-08-31 19:30:52 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local success,returned=pcall(function()
 
-local startTime=tick()
+local startTime=tick() --// it took me 1 hour to figure out how to find the loading time
 
 local code=[[
 	return (loadstring or load)(game:HttpGet('https://raw.githubusercontent.com/biteymeow/VapeV4ForRobloxMods/refs/heads/main/mods/17750024818.lua', true))()
@@ -34,7 +34,7 @@ end
 
 local success,returned=pcall(function()
 
-local startTime=tick()
+local startTime=tick() --// WHY THE FUCK DOES VAPEV4 TAKE 11 SECONDS TO LOAD, xylex fix ur shit
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua', true))()
 
@@ -61,3 +61,5 @@ Title='Vape V4 For Roblox Mods';
 Text='Failed to load Vape V4 Error: ' .. returned})
 
 end
+
+--// spooky month is a good show, i think there should be a episode of a sprite creature...
