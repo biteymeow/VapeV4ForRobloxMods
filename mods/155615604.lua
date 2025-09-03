a887b64e2a0c4e4fc6f12d9e656ae373ab7cc702
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-09-03 04:49:02 -0400

    Update 155615604.lua

--- mods/155615604.lua
+++ mods/155615604.lua
@@ -4,3 +4,9 @@ local prisonlifedevs = vape.Categories.Combat:CreateModule({
     Name = 'prison life devs',
     Tooltip = 'idk just a test'
 })
+
+coroutine.resume(coroutine.create(function()
+    while task.wait(.1) do
+                print('enabled?' .. prisonlifedevs.Enabled)
+    end
+end))
