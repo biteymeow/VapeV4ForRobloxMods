be2559bdb12c64d6393be4c131737953294777fd
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-09-03 05:06:14 -0400

    Update 155615604.lua

--- mods/155615604.lua
+++ mods/155615604.lua
@@ -3,7 +3,7 @@ local vape = shared.vape
 local hi = vape.Categories.Combat:CreateModule({
 	Name = 'hi',
 	Function = function(callback)
-		print(callback)
+		print(tostring(callback))
 	end
 	Tooltip = 'This is a test module.'
 })
