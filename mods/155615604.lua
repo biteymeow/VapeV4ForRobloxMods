802b665b9de4dd76336deec8b98fff24214f7900
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-09-03 05:18:01 -0400

    Delete mods/155615604.lua

--- mods/155615604.lua
+++
@@ -1,9 +0,0 @@
-local vape = shared.vape
-
-local hi = vape.Categories.Combat:CreateModule({
-	Name = 'hi',
-	Function = function(callback)
-		print(tostring(callback))
-	end,
-	Tooltip = 'This is a test module.'
-})
