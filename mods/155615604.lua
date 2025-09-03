48e568ad7ec077c03176b0cde14d29f95e75dcb3
sharkymeow the sprite drinker <100092400+sharkymeowwww@users.noreply.github.com>
2025-09-03 05:01:08 -0400

    Update 155615604.lua

--- mods/155615604.lua
+++ mods/155615604.lua
@@ -1,16 +1,9 @@
 local vape = shared.vape
 
-local SilentAim
-SilentAim = vape.Categories.Combat:CreateModule({
-	Name = 'SilentAim',
+local hi = vape.Categories.Combat:CreateModule({
+	Name = 'hi',
 	Function = function(callback)
-		print(callback, 'module state')
-		SilentAim:Clean(Instance.new('Part'))
-		repeat
-			print('repeat loop!')
-			task.wait(1)
-		until (not SilentAim.Enabled)
-	end,
-	ExtraText = function() return 'Test' end,
+		print(callback)
+	end
 	Tooltip = 'This is a test module.'
 })
